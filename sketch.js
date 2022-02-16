7912eeffeb9f66b5391dba24bfac52be6eb12ece
magarini <56253774+magarini@users.noreply.github.com>
2022-02-16 18:11:42 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+// let song;
+
+// function setup() {
+//   song = loadSound('spacebar.wav');
+  
+// }
+
+// function mousePressed() {
+//   if (song.isPlaying()) {
+//     // .isPlaying() returns a boolean
+//     song.stop();
+//   } else {
+//     song.play();
+//   }
+// }
+
+
+var song;
+var button;
+var jumpButton;
+let canvas;
+
+function preload(){
+    song = loadSound('spacebar.wav', loaded);
+}
+
+
+function setup() {
+
+    select('#playbutton').mousePressed(togglePlaying);
+    let elt = document.getElementById('radio');
+    canvas.parent(elt);
+}
+
+function togglePlaying() {
+    select('#playbutton').hide();
+    if (!song.isPlaying()) {
+      song.play();
+      song.setVolume(0.3);
+      select('#playbutton').html('sound:on');
+    } else {
+      song.stop();
+      select('#playbutton').html('sound:off');
+    }
+  }
+  
+
+function loaded() {
+    console.log('loaded');
+  }
